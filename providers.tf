134790af62638e680488f580fda91c4df3da3edf
Josephine Chioma Mbuagbaw <mbuchioma@gmail.com>
2022-12-16 11:52:33 -0500

    Configuring the provider

---
+++ providers.tf
@@ -0,0 +1,14 @@
+# Configure the provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
