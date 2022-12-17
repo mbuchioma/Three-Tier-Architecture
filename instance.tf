267a703ab35b7595c675ad99fb215d3bbdda77ac
mbuchioma <mbuchioma@gmail.com>
2022-12-17 08:30:02 -0500

    DB instances created

--- instance.tf
+++ instance.tf
@@ -1,12 +1,14 @@
 # Creating RDS instances
-# resource "aws_db_instance" "default" {
-#   allocated_storage    = 10
-#   db_name              = "mydb"
-#   engine               = "mysql"
-#   engine_version       = "5.7"
-#   instance_class       = "db.t3.micro"
-#   username             = "foo"
-#   password             = "foobarbaz"
-#   parameter_group_name = "default.mysql5.7"
-#   skip_final_snapshot  = true
-# }
+ resource "aws_db_instance" "my-db" {
+    count = 2
+   allocated_storage    = 10
+   db_name              = "mydb${count.index+1}"
+   engine               = "mysql"
+   engine_version       = "5.7"
+   instance_class       = "db.t2.micro"
+   username             = "admin12345"
+   password             = "admin12345"
+   parameter_group_name = "default.mysql5.7"
+   skip_final_snapshot  = true
+   db_subnet_group_name = aws_db_subnet_group.my_db_subnet-group.name
+ }
